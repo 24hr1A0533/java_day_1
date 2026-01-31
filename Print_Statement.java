31cbeb0f2c4604f7ce4638a8f89931bc811357d6
C S KISHORE ROYAL <kingsofmcg@gmail.com>
2026-01-31 14:27:23 +0530

    Add files via upload

---
+++ Print_Statement.java
@@ -0,0 +1,7 @@
+public class Print_Statement {
+    public static void main(String[]args){
+        System.out.println("hello world");
+        System.out.print( "i love india");
+        System.out.println("  i am indian");
+    }
+}
