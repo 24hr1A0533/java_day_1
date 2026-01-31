30153f194d815aebf204698fd163f2ae3c7f24ce
C S KISHORE ROYAL <kingsofmcg@gmail.com>
2026-01-31 14:58:39 +0530

    Add files via upload

---
+++ Scanner_Class.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+public class Scanner_Class {
+    public static void main(String[]args){
+        Scanner data = new Scanner (System.in);
+        System.out.println("enter a value:");
+        int a=data.nextInt();
+        System.out.println("value of a is :"+a);
+
+    }
+}
