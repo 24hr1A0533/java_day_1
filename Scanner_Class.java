8fe05022f21e717b459dce00aade1a430f1b66f8
C S KISHORE ROYAL <kingsofmcg@gmail.com>
2026-02-03 23:35:26 +0530

    Update Scanner_Class.java

--- Scanner_Class.java
+++ Scanner_Class.java
@@ -3,9 +3,32 @@ import java.util.Scanner;
 public class Scanner_Class {
     public static void main(String[]args){
         Scanner data = new Scanner (System.in);
-        System.out.println("enter a value:");
-        int a=data.nextInt();
-        System.out.println("value of a is :"+a);
+       byte a =data.nextByte();
+        System.out.println(a);
+
+        short b =data.nextShort();
+        System.out.println(b);
+
+        int c = data.nextInt();
+        System.out.println(c);
+
+        long d = data.nextLong();
+        System.out.println(d);
+
+        float e = data.nextFloat();
+        System.out.println(e);
+
+        double f = data.nextDouble();
+        System.out.println(f);
+
+        String ap=data.next();
+        System.out.println(ap);
+        data.nextLine();
+
+        String str1=data.nextLine();
+        System.out.println(str1);
+
 
     }
 }
+
